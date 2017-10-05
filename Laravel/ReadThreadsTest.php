d2dce23f5db6d6a58e67876df668fbe04803bce5
Przemek Raczynski <p.raczynski@gmail.com>
2017-10-05 09:33:36 +0100

    Add example test class

---
+++ Laravel/ReadThreadsTest.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Reply;
+use App\Thread;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ReadThreadsTest extends TestCase
+{
+    protected $thread;
+    use DatabaseMigrations;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $this->thread = create('App\Thread');
+    }
+
+    /** @test */
+    public function a_user_can_view_all_threads()
+    {
+        $this->get('/threads')
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_see_a_thread()
+    {
+        $this->get($this->thread->path())
+            ->assertSee($this->thread->title);
+    }
+
+    /** @test */
+    public function a_user_can_see_replies_which_are_associated_with_a_thread()
+    {
+        $reply = create('App\Reply', ['thread_id' => $this->thread->id]);
+
+        $this->get($this->thread->path())
+            ->assertSee($reply->body);
+    }
+
+    /** @test */
+    public function a_user_can_filter_threads_according_to_a_channel()
+    {
+        $channel = create('App\Channel');
+        $threadInChannel = create('App\Thread', ['channel_id' => $channel->id]);
+        $threadNonInChannel = create('App\Thread');
+
+        $this->get('threads/' . $channel->slug)
+            ->assertSee($threadInChannel->title)
+            ->assertDontSee($threadNonInChannel->title);
+    }
+
+    /** @test */
+    public function a_user_can_filter_threads_by_any_username()
+    {
+        $this->signIn(create('App\User', ['name' => 'JohnDoe']));
+
+        $threadByJohn = create('App\Thread', ['user_id' => auth()->user()->id ]);
+
+        $threadNotByJohn = create('App\Thread');
+
+        $this->get('threads?by=JohnDoe')
+                ->assertSee($threadByJohn->title)
+                ->assertDontSee($threadNotByJohn->title);
+    }
+
+    /** @test */
+    public function a_user_can_filter_threads_by_popularity()
+    {
+        $threadWithTwoReplies = create(Thread::class);
+        create(Reply::class, ['thread_id' => $threadWithTwoReplies], 2);
+
+        $threadWithThreeReplies = create(Thread::class);
+        create(Reply::class, ['thread_id' => $threadWithThreeReplies], 3);
+
+        $threadWithNoReplies = $this->thread;
+
+        $response = $this->getJson('threads?popular=1')->json();
+
+        $this->assertEquals([3, 2, 0], array_column($response, 'replies_count'));
+    }
+
+}
